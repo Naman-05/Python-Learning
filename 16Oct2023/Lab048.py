ddd3d50cace921ed7fc12f72ed743dc7f2e15356
Naman-05 <singhalnaman94@gmail.com>
2024-05-10 22:08:57 +0530

    16Oct2023

---
+++ 16Oct2023/Lab048.py
@@ -0,0 +1,13 @@
+x = list(range(10))
+print(x) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for x in range(10):
+    print(x)
+
+x = list(range(0,-10,-1))
+print(x) #[0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+
+
+
+for x in range(12,20):
+    print(x)
